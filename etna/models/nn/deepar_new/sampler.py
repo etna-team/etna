60f374aca168ef8ffff7d57c75cd19c68442a93c
Egor Baturin <egoriyaa@github.com>
2023-11-08 02:11:15 +0300

    fix

--- etna/models/nn/deepar_new/sampler.py
+++ etna/models/nn/deepar_new/sampler.py
@@ -1,6 +1,5 @@
 from etna import SETTINGS
 
-N_SAMPLES = 500000
 if SETTINGS.torch_required:
     import torch
     from torch.utils.data.sampler import Sampler
@@ -9,16 +8,17 @@ if SETTINGS.torch_required:
 class WeightedDeepARSampler(Sampler):
     """Samples batch elements with probabilities `1 + mean`, where mean is a mean of target values in batch."""
 
-    def __init__(self, data):
+    def __init__(self, data, n_samples):
         self.data = data
+        self.n_samples = n_samples
 
     def __iter__(self):
         weights = torch.tensor([sample["weight"] for sample in self.data])
-        idx = torch.multinomial(1 / weights, num_samples=N_SAMPLES, replacement=False)  # TODO ok?
+        idx = torch.multinomial(1 / weights, num_samples=self.n_samples, replacement=False)  # TODO ok?
         return iter(idx)
 
     def __len__(self):
-        return N_SAMPLES
+        return self.n_samples
 
 
 class SamplerWrapper:
