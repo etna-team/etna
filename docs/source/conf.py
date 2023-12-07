b8ad8b09e6887278086ebd0049f27ad593455aeb
Yakov Malyshev <38911542+ostreech1997@users.noreply.github.com>
2023-12-07 13:20:19 +0300

    Create page about internal datasets in documentation (#175)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,6 +61,7 @@ extensions = [
     "sphinx.ext.mathjax",  # render math via JavaScript
     "sphinx.ext.githubpages",  # creates .nojekyll file
     "sphinx.ext.linkcode",  # add external links to source code
+    "sphinx.ext.autosectionlabel"  # add internal links in docs
 ]
 
 # List of patterns, relative to source directory, that match files and
