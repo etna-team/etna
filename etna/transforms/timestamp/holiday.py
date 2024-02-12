853564be36044ed9b41102eead7efaafe95880aa
Grigory Zlotin <grigory.zlotin@gmail.com>
2024-02-12 22:54:11 +0300

    holiday_tranform

--- etna/transforms/timestamp/holiday.py
+++ etna/transforms/timestamp/holiday.py
@@ -1,4 +1,6 @@
-import datetime
+import itertools
+from datetime import datetime
+from datetime import timedelta
 from enum import Enum
 from typing import List
 from typing import Optional
@@ -15,6 +17,7 @@ class HolidayTransformMode(str, Enum):
 
     binary = "binary"
     category = "category"
+    days_count = "days_count"
 
     @classmethod
     def _missing_(cls, value):
@@ -27,7 +30,7 @@ class HolidayTransform(IrreversibleTransform):
     """
     HolidayTransform generates series that indicates holidays in given dataset.
 
-    In ``binary`` mode shows the presence of holiday in that day. In ``category`` mode shows the name of the holiday
+    In `binary` mode shows the presence of holiday in that day. In `category` mode shows the name of the holiday
     with value "NO_HOLIDAY" reserved for days without holidays.
     """
 
@@ -42,9 +45,9 @@ class HolidayTransform(IrreversibleTransform):
         iso_code:
             internationally recognised codes, designated to country for which we want to find the holidays
         mode:
-            `binary` to indicate holidays, `category` to specify which holiday do we have at each day
+            binary to indicate holidays, category to specify which holiday do we have at each day
         out_column:
-            name of added column. Use ``self.__repr__()`` if not given.
+            name of added column. Use `self.__repr__()` if not given.
         """
         super().__init__(required_features=["target"])
         self.iso_code = iso_code
@@ -73,6 +76,7 @@ class HolidayTransform(IrreversibleTransform):
     def _transform(self, df: pd.DataFrame) -> pd.DataFrame:
         """
         Transform data from df with HolidayTransform and generate a column of holidays flags or its titles.
+        In days_count mode calculates the frequency of holidays in a given period of time
 
         Parameters
         ----------
@@ -84,13 +88,40 @@ class HolidayTransform(IrreversibleTransform):
         :
             pd.DataFrame with added holidays
         """
-        if (df.index[1] - df.index[0]) > datetime.timedelta(days=1):
-            raise ValueError("Frequency of data should be no more than daily.")
+        inferred_freq = pd.infer_freq(df.index)
+        if inferred_freq != "D" and self._mode is not HolidayTransformMode.days_count:
+            raise ValueError("In default mode frequency of data should be no more than daily.")
 
         cols = df.columns.get_level_values("segment").unique()
         out_column = self._get_column_name()
 
-        if self._mode is HolidayTransformMode.category:
+        if self._mode is HolidayTransformMode.days_count:
+            encoded_matrix = np.empty(0)
+            for date in df.index:
+                numbers_part = "".join(itertools.takewhile(str.isdigit, inferred_freq))
+                letters_part = "".join(itertools.dropwhile(str.isdigit, inferred_freq))
+                numbers_part = "1" if numbers_part == "" else numbers_part
+                start_date = datetime(2011, 1, 1)
+                new_date = datetime(2011, 1, 1)
+                if letters_part == "W":
+                    start_date = date - timedelta(days=date.weekday())
+                    new_date = start_date + pd.DateOffset(weeks=int(numbers_part))
+                elif letters_part in ["M", "MS"]:
+                    start_date = date.replace(day=1)
+                    new_date = start_date + pd.DateOffset(months=int(numbers_part))
+                elif letters_part in ["Q-DEC", "QS-OCT", "QS-DEC", "QS-OST"]:
+                    start_date = date.replace(day=1)
+                    new_date = start_date + pd.DateOffset(months=3 * int(numbers_part))
+                elif letters_part in ["A-DEC", "AS-JAN"]:
+                    start_date = date.replace(month=1, day=1)
+                    new_date = start_date + pd.DateOffset(years=int(numbers_part))
+                else:
+                    raise ValueError("days_count mode don't support frequency less than Weekly")
+                date_range = pd.date_range(start=start_date, end=(new_date - timedelta(days=1)), freq="d")
+                count_holidays = sum(1 for d in date_range if d in self.holidays)
+                holidays_freq = count_holidays / date_range.size
+                encoded_matrix = np.append(encoded_matrix, holidays_freq)
+        elif self._mode is HolidayTransformMode.category:
             encoded_matrix = np.array(
                 [self.holidays[x] if x in self.holidays else self._no_holiday_name for x in df.index]
             )
@@ -102,8 +133,8 @@ class HolidayTransform(IrreversibleTransform):
             columns=pd.MultiIndex.from_product([cols, [out_column]], names=("segment", "feature")),
             index=df.index,
         )
-        encoded_df = encoded_df.astype("category")
-
+        if self._mode is HolidayTransformMode.days_count:
+            encoded_df = encoded_df.astype("category")
         df = df.join(encoded_df)
         df = df.sort_index(axis=1)
         return df
