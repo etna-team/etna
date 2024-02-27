f00ef07282e181d4fce6ff9d5a1f705567c10cc1
Egor Baturin <egoriyaa@github.com>
2024-02-27 05:16:22 +0300

    delete unnecessary utils

--- etna/libs/ts2vec/encoder.py
+++ etna/libs/ts2vec/encoder.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 import numpy as np
-from .dilated_conv import DilatedConvEncoder
+from etna.libs.ts2vec.dilated_conv import DilatedConvEncoder
 
 
 def generate_continuous_mask(B, T, n=5, l=0.1):
--- etna/libs/ts2vec/utils.py
+++ etna/libs/ts2vec/utils.py
@@ -6,16 +6,6 @@ from datetime import datetime
 from copy import deepcopy
 
 
-def pkl_save(name, var):
-    with open(name, 'wb') as f:
-        pickle.dump(var, f)
-
-
-def pkl_load(name):
-    with open(name, 'rb') as f:
-        return pickle.load(f)
-
-
 def torch_pad_nan(arr, left=0, right=0, dim=0):
     if left > 0:
         padshape = list(arr.shape)
@@ -63,78 +53,6 @@ def centerize_vary_length_series(x):
     offset[offset < 0] += x.shape[1]
     column_indices = column_indices - offset[:, np.newaxis]
     return x[rows, column_indices]
-
-
-def data_dropout(arr, p):
-    B, T = arr.shape[0], arr.shape[1]
-    mask = np.full(B * T, False, dtype=np.bool)
-    ele_sel = np.random.choice(
-        B * T,
-        size=int(B * T * p),
-        replace=False
-    )
-    mask[ele_sel] = True
-    res = arr.copy()
-    res[mask.reshape(B, T)] = np.nan
-    return res
-
-
-def name_with_datetime(prefix='default'):
-    now = datetime.now()
-    return prefix + '_' + now.strftime("%Y%m%d_%H%M%S")
-
-
-def init_dl_program(
-        device_name,
-        seed=None,
-        use_cudnn=True,
-        deterministic=False,
-        benchmark=False,
-        use_tf32=False,
-        max_threads=None
-):
-    import torch
-    if max_threads is not None:
-        torch.set_num_threads(max_threads)  # intraop
-        if torch.get_num_interop_threads() != max_threads:
-            torch.set_num_interop_threads(max_threads)  # interop
-        try:
-            import mkl
-        except:
-            pass
-        else:
-            mkl.set_num_threads(max_threads)
-
-    if seed is not None:
-        random.seed(seed)
-        seed += 1
-        np.random.seed(seed)
-        seed += 1
-        torch.manual_seed(seed)
-
-    if isinstance(device_name, (str, int)):
-        device_name = [device_name]
-
-    devices = []
-    for t in reversed(device_name):
-        t_device = torch.device(t)
-        devices.append(t_device)
-        if t_device.type == 'cuda':
-            assert torch.cuda.is_available()
-            torch.cuda.set_device(t_device)
-            if seed is not None:
-                seed += 1
-                torch.cuda.manual_seed(seed)
-    devices.reverse()
-    torch.backends.cudnn.enabled = use_cudnn
-    torch.backends.cudnn.deterministic = deterministic
-    torch.backends.cudnn.benchmark = benchmark
-
-    if hasattr(torch.backends.cudnn, 'allow_tf32'):
-        torch.backends.cudnn.allow_tf32 = use_tf32
-        torch.backends.cuda.matmul.allow_tf32 = use_tf32
-
-    return devices if len(devices) > 1 else devices[0]
 
 
 class AveragedModel(torch.optim.swa_utils.AveragedModel):
