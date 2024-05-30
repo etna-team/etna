ad2a3ce02b07112f82bacb2181575a83d8d39096
brsnw250 <maxzherelo1@gmail.com>
2024-05-30 17:21:36 +0300

    added iqr method

--- etna/analysis/__init__.py
+++ etna/analysis/__init__.py
@@ -42,3 +42,4 @@ from etna.analysis.outliers.density_outliers import get_anomalies_density
 from etna.analysis.outliers.hist_outliers import get_anomalies_hist
 from etna.analysis.outliers.median_outliers import get_anomalies_median
 from etna.analysis.outliers.prediction_interval_outliers import get_anomalies_prediction_interval
+from etna.analysis.outliers.rolling_statistics import get_anomalies_iqr
