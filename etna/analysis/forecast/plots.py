5967a848c71c1be18d69fc78771ea93675ba0c71
Mikhail <m.bolev@tinkoff.ru>
2024-06-24 15:49:46 +0300

    fix: remove sorting of ts.df by timestamps in plot methods (#389)

--- etna/analysis/forecast/plots.py
+++ etna/analysis/forecast/plots.py
@@ -124,11 +124,6 @@ def plot_forecast(
     if prediction_intervals:
         prediction_intervals_names = _select_prediction_intervals_names(forecast_results, quantiles)
 
-    if train_ts is not None:
-        train_ts.df.sort_values(by="timestamp", inplace=True)
-    if test_ts is not None:
-        test_ts.df.sort_values(by="timestamp", inplace=True)
-
     for i, segment in enumerate(segments):
         if train_ts is not None:
             segment_train_df = train_ts[:, segment, :][segment]
@@ -913,9 +908,6 @@ def plot_forecast_decomposition(
         num_plots = math.ceil(len(segments) / columns_num) * columns_num * (len(components) + 1)
 
     _, ax = _prepare_axes(num_plots=num_plots, columns_num=columns_num, figsize=figsize, set_grid=show_grid)
-
-    if test_ts is not None:
-        test_ts.df.sort_values(by="timestamp", inplace=True)
 
     alpha = 0.5 if components_mode == ComponentsMode.joint else 1.0
     ax_array = np.asarray(ax).reshape(-1, columns_num).T.ravel()
