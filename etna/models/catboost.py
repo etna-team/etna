4b291bba0973aefd1cd507f4320297e80667ce8d
Artem Liakhov <Liakhov.Artem@yandex.ru>
2024-08-13 22:17:13 +0300

    Add `kwargs` parameter description for `CatBoostMultiSegmentModel`

--- etna/models/catboost.py
+++ etna/models/catboost.py
@@ -424,6 +424,8 @@ class CatBoostMultiSegmentModel(
             * For GPU. The given value is used for reading the data from the hard drive and does
               not affect the training.
               During the training one main thread and one thread for each GPU are used.
+        **kwargs:
+            Additional parameters passed to `CatBoostRegressor` model.
         """
         self.iterations = iterations
         self.depth = depth
