760a6e9c8c74b360da9ba401b09d3c5ddf8e29c5
d-a-bunin <142778107+d-a-bunin@users.noreply.github.com>
2025-02-20 13:23:57 +0300

    Fix `ConfigSampler` to handle trials without hash (#616)

--- etna/auto/optuna/config_sampler.py
+++ etna/auto/optuna/config_sampler.py
@@ -14,7 +14,13 @@ from etna.auto.utils import retry
 
 
 class ConfigSampler(BaseSampler):
-    """Optuna based sampler for greedy search over different configurations."""
+    """Optuna based sampler for greedy search over different configurations.
+
+    The core difference with :py:class:`optuna.samplers.GridSampler` is that we want to get current trial params
+    (``params`` + ``relative_params``) in objective function.
+    But :py:class:`optuna.samplers.GridSampler` does not allow this, because it only implements ``sample_independent``
+    which isn't suitable for this.
+    """
 
     def __init__(self, configs: List[dict], random_generator: Optional[np.random.Generator] = None, retries: int = 10):
         """Init Config sampler.
@@ -59,8 +65,9 @@ class ConfigSampler(BaseSampler):
         trials_to_sample = self._get_unfinished_hashes(study=study, current_trial=trial)
 
         if len(trials_to_sample) == 0:
-            # TODO: this could cause job duplication
-            # For some reason `_get_unfinished_hashes` does not return zero length list in `after_trial`
+            # This case may occur with distributed optimization or trial queue. If there is no
+            # target grid, `ConfigSampler` evaluates a visited, duplicated point with the current
+            # trial. After that, the optimization stops.
             _to_sample = list(self.configs_hash)
             idx = self.rng.choice(len(_to_sample))
             hash_to_sample = _to_sample[idx]
@@ -69,11 +76,10 @@ class ConfigSampler(BaseSampler):
             idx = self.rng.choice(len(_trials_to_sample))
             hash_to_sample = _trials_to_sample[idx]
 
-        map_to_objective = self.configs_hash[hash_to_sample]
-
+        pipeline = self.configs_hash[hash_to_sample]
         study._storage.set_trial_user_attr(trial._trial_id, "hash", hash_to_sample)
-        study._storage.set_trial_user_attr(trial._trial_id, "pipeline", map_to_objective)
-        return map_to_objective
+        study._storage.set_trial_user_attr(trial._trial_id, "pipeline", pipeline)
+        return pipeline
 
     def after_trial(self, study: Study, trial: FrozenTrial, *args, **kwargs) -> None:  # noqa: D102
         """Stop study if all configs have been tested.
@@ -84,10 +90,11 @@ class ConfigSampler(BaseSampler):
             current optuna study
         """
         unfinished_hashes = self._get_unfinished_hashes(study=study, current_trial=trial)
+        hash_to_sample = trial.user_attrs["hash"]
 
         if len(unfinished_hashes) == 0:
             study.stop()
-        if len(unfinished_hashes) == 1 and list(unfinished_hashes)[0] == trial.user_attrs["hash"]:
+        if len(unfinished_hashes) == 1 and unfinished_hashes.pop() == hash_to_sample:
             study.stop()
 
     def _get_unfinished_hashes(self, study: Study, current_trial: Optional[FrozenTrial] = None) -> Set[str]:
@@ -103,6 +110,9 @@ class ConfigSampler(BaseSampler):
         :
             hashes to run
         """
+        # We directly query the storage to get trials here instead of `study.get_trials`,
+        # since some pruners such as `HyperbandPruner` use the study transformed
+        # to filter trials. See https://github.com/optuna/optuna/issues/2327 for details.
         trials = study._storage.get_all_trials(study._study_id, deepcopy=False)
 
         if current_trial is not None:
@@ -117,14 +127,24 @@ class ConfigSampler(BaseSampler):
             elif t.state == TrialState.RUNNING:
 
                 def _closure(trial):
-                    return study._storage.get_trial(trial._trial_id).user_attrs["hash"]
+                    return study._storage.get_trial(trial._trial_id).user_attrs.get("hash")
 
                 hash_to_add = retry(partial(_closure, trial=t), max_retries=self.retries)
-                running_trials_hash.append(hash_to_add)
+                # if we don't get hash, it could lead to duplication
+                if hash_to_add is not None:
+                    running_trials_hash.append(hash_to_add)
             else:
                 pass
 
-        return set(self.configs_hash) - set(finished_trials_hash) - set(running_trials_hash)
+        unfinished_hash = set(self.configs_hash) - set(finished_trials_hash) - set(running_trials_hash)
+
+        # If evaluations for all hashes have been started, return hashes that have not yet finished
+        # because all hashes should be evaluated before stopping the optimization.
+        # This logic is copied from `GridSampler`
+        if len(unfinished_hash) == 0:
+            unfinished_hash = set(self.configs_hash) - set(finished_trials_hash)
+
+        return unfinished_hash
 
     @property
     def rng(self):  # noqa: D102
