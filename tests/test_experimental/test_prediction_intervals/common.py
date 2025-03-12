1e0f91da90204cfb2a5752bf6f7448462cbc22a3
Danil Smorchkov <Danil Smorchkov>
2025-03-12 11:30:05 +0300

    change .columns to ._df.columns

--- tests/test_experimental/test_prediction_intervals/common.py
+++ tests/test_experimental/test_prediction_intervals/common.py
@@ -40,7 +40,7 @@ def run_base_pipeline_compat_check(ts, intervals_pipeline, expected_columns):
     intervals_pipeline.fit(ts=ts)
 
     intervals_pipeline_pred = intervals_pipeline.forecast(prediction_interval=True)
-    columns = intervals_pipeline_pred.columns.get_level_values("feature")
+    columns = intervals_pipeline_pred._df.columns.get_level_values("feature")
 
     assert len(expected_columns - set(columns)) == 0
     assert np.sum(intervals_pipeline_pred._df.isna().values) == 0
@@ -60,7 +60,7 @@ class DummyPredictionIntervals(BasePredictionIntervals):
         borders = []
         for segment in ts.segments:
             target_df = (predictions[:, segment, "target"]).to_frame()
-            target_df.columns.names = predictions.columns.names
+            target_df.columns.names = predictions._df.columns.names
             borders.append(target_df.rename({"target": f"target_lower"}, axis=1) - self.width / 2)
             borders.append(target_df.rename({"target": f"target_upper"}, axis=1) + self.width / 2)
 
