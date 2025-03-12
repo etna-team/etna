1ab14b409b99cbccd03c678c53c5cbc656f571d2
Danil Smorchkov <Danil Smorchkov>
2025-03-12 11:50:43 +0300

    lint

--- tests/test_transforms/test_encoders/test_mean_encoder_transform.py
+++ tests/test_transforms/test_encoders/test_mean_encoder_transform.py
@@ -363,7 +363,9 @@ def test_ts_begin_nan_smooth_1(ts_begin_nan, expected_ts_begin_nan_smooth_1):
     )
     mean_encoder.fit_transform(ts_begin_nan)
     assert_frame_equal(
-        ts_begin_nan._df.loc[:, pd.IndexSlice[:, "mean_encoded_regressor"]], expected_ts_begin_nan_smooth_1._df, atol=0.01
+        ts_begin_nan._df.loc[:, pd.IndexSlice[:, "mean_encoded_regressor"]],
+        expected_ts_begin_nan_smooth_1._df,
+        atol=0.01,
     )
 
 
@@ -377,7 +379,9 @@ def test_ts_begin_nan_smooth_2(ts_begin_nan, expected_ts_begin_nan_smooth_2):
     )
     mean_encoder.fit_transform(ts_begin_nan)
     assert_frame_equal(
-        ts_begin_nan._dfloc[:, pd.IndexSlice[:, "mean_encoded_regressor"]], expected_ts_begin_nan_smooth_2._df, atol=0.01
+        ts_begin_nan._dfloc[:, pd.IndexSlice[:, "mean_encoded_regressor"]],
+        expected_ts_begin_nan_smooth_2._df,
+        atol=0.01,
     )
 
 
